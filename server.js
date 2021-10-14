9e209d446a8c9474e66de36dc9f86d1fb59b462a
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-10-14 19:51:27 -0400

    Error handler to throw back to index.html working. Started get and post for notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const db = require("./db/db.json");
 
 const PORT = 3001;
 
@@ -12,7 +13,19 @@ app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-})
+});
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// app.get("/api/notes", (req, res) => {
+//     res.json(db);
+//     console.info(db);
+// });
+
+// app.post()
+
 
 
 app.listen(PORT, () =>
