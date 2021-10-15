353fd1c652cfcffc613f934f21074e0312c86a0f
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-10-14 23:25:07 -0400

    Error handler is breaking the notes json and is disabled for now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
@@ -15,16 +15,11 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// app.get("/api/notes", (req, res) => {
-//     res.json(db);
-//     console.info(db);
+// app.get("*", (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, "/public/index.html"));
 // });
 
-// app.post()
+app.get('/api', (req, res) => res.json(db));
 
 
 
