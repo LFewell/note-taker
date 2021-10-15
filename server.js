4df71e5cdfd421c0746858c10d628edebaaeb8eb
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-10-15 09:35:20 -0400

    Used fs to get the notes from api and posting of new notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
 const fs = require("fs");
+const nid = require("nid");
 
 const PORT = process.env.port || 3001;
 
@@ -20,22 +21,31 @@ app.get("/notes", (req, res) => {
 //     res.status(404).sendFile(path.join(__dirname, "/public/index.html"));
 // });
 
-app.get("/api/notes", (req, res) => res.json(db))
-
+app.get("/api/notes", (req, res) => {
+    const notes = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+    let parse = JSON.parse(notes);
+    res.json(parse);
+})
 
 app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (data) => {
-        const newNote = req.body;
-        const parsedNote = JSON.parse(data);
-        parsedNote.push(newNote);
-        fs.writeFile(
-            "./db/db.json",
-            JSON.stringify(newNote)
-        )
+    const db = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+    let parse = JSON.parse(db);
+    let newNote = req.body;
+    newNote.id = nid();
 
-    })
-    
+    parse.push(newNote);
+    let newDb = JSON.stringify(parse);
+
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"), newDb);
+    res.json(newNote);
 })
+
+// app.post("/api/notes", (req, res) => {
+//     const newNote = req.body;
+//     newNote.id = nid();
+//     db.push(newNote);
+//     console.log(newNote);
+// })
 
 
 
